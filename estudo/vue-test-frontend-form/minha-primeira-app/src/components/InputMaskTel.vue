1f9adc016181105e4c77e69eb23923c22f79f46d
dloopes <dl@loopes.com.br>
2020-07-29 19:33:04 -0300

    create mask tel input component

---
+++ estudo/vue-test-frontend-form/minha-primeira-app/src/components/InputMaskTel.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <the-mask :mask="['(##) ####-####', '(##) #####-####']" />
+</div>
+</template>
+
+<script>
+import {TheMask} from 'vue-the-mask'
+
+export default {
+  name: 'InputMaskTel',
+  props: {
+    mask: Number
+  },
+  components: {TheMask}
+}
+</script>
+
+<style>
+
+</style>
