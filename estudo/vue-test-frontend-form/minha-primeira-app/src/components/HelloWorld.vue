04c82e3881d978ba41edeea49a2417d98329352d
dloopes <dl@loopes.com.br>
2020-07-29 14:35:27 -0300

    Subindo Projeto V-Form-Builder

---
+++ estudo/vue-test-frontend-form/minha-primeira-app/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+ <form-builder type="template"></form-builder>
+</div>
+</template>
+
+<script>
+import FormBuilder from 'v-form-builder';
+
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components:{FormBuilder}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
